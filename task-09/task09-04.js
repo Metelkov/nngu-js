218f7030fe611a748de00f56d6fcb3bba777ec68
Alexey Metelkov <al.metelkov@gmail.com>
2023-11-16 21:51:51 +0300

    work 9

--- task-09/task09-04.js
+++ task-09/task09-04.js
@@ -1,41 +1,42 @@
 
 //4. Изменить код метода wrapperA  таким образом, чтобы добавлялся анкор (текст) ссылки. Текст // передавать массивом.
 
+// function Menu(...navList){
+//     this.navList=navList;
+//     this.wrapperA=function(){
+//         return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
+//     }
+// }
+// let mainMenu=new Menu("home.html","services.html","price.html","about.html");
+// for (elem of mainMenu.navList)
+//     console.log(elem);
+//     console.log(mainMenu.wrapperA());
+// let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
+// for (elem of footerMenu.navList)
+//     console.log(elem);
+//     console.log(footerMenu.wrapperA()); 
+
+
+//нужно добавить i для перебора, организуем некий цикл
+
+
 function Menu(...navList){
     this.navList=navList;
-    this.wrapperA=function(){
-        return this.navList.map(elem=>`<a href='${elem}'>item</a>`);
+    this.wrapperA=function(...anchor){
+        let i = 0;
+        return this.navList.map(function(elem) {
+            let str = `<a href='${elem}'>${anchor[i]}</a>`;
+            i++;
+            return str;
+        });
     }
 }
 let mainMenu=new Menu("home.html","services.html","price.html","about.html");
 for (elem of mainMenu.navList)
     console.log(elem);
-    console.log(mainMenu.wrapperA());
+    console.log(mainMenu.wrapperA("Домой", "Услуги", "Цены", "О нас"));
 let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
 for (elem of footerMenu.navList)
     console.log(elem);
-    console.log(footerMenu.wrapperA()); 
-
-
-///нужно вставить в return где объявляется ссылка что то, this и какой то метод
-//перебора массива
-//скучно перебирать и подбирать
-
-
-function Menu(...navList){
-    this.navList=navList;
-    this.wrapperA=function(){
-        return this.navList.map(elem=>`<a href='${this.Menu}'>item</a>`+"");
-    }
-}
-let mainMenu=new Menu("home.html","services.html","price.html","about.html");
-for (elem of mainMenu.navList)
-    console.log(elem);
-    console.log(mainMenu.wrapperA());
-let footerMenu=new Menu("newPage.html","checkPage.html","seoPage.html");
-for (elem of footerMenu.navList)
-    console.log(elem);
-    console.log(footerMenu.wrapperA()); 
-
-
+    console.log(footerMenu.wrapperA("Создание страниц", "Проверка страниц", "Сеонизация страниц"));
 
