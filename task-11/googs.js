a94bf8181515786ebd1c8f68bab18ac79116b034
Alexey Metelkov <al.metelkov@gmail.com>
2023-11-27 21:23:30 +0300

    hw 11

---
+++ task-11/googs.js
@@ -0,0 +1,42 @@
+'use strict'
+export let goodsArr = [
+    {
+        name:"11",
+        imgSrc:"1.jpg",
+        corpus:"black",
+        remen:"black",
+        displaySize:"0.96\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 4,2",
+            os2: "iOS 8",
+        }
+    },
+    {
+        name:"22",
+        imgSrc:"2.jpg",
+        corpus:"black",
+        remen:"black",
+        displaySize:"0.96\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 4,2",
+            os2: "iOS 8",
+        }
+    },
+    {
+        name:"33",
+        imgSrc:"3.jpg",
+        corpus:"black",
+        remen:"black",
+        displaySize:"0.96\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 4,2",
+            os2: "iOS 8",
+        }
+    },
+];
---
+++ task-11/typograph.js
@@ -0,0 +1,9 @@
+'use strict'
+export let typograph = {
+        bwSheetType:100,
+        bwSheetCopy:90,
+        colorSheetType:200,
+        colorSheetCopy:190,
+        photo20x40:150,
+        photo50x60:240,
+    };
