e63aeec85b5de942d0c27a71d478f0140eac683b
Alexey Metelkov <al.metelkov@gmail.com>
2023-11-30 21:37:49 +0300

    some add

---
+++ task-11/goods.js
@@ -0,0 +1,42 @@
+'use strict'
+export let goods = [
+    {
+        name:"11",
+        imgSrc:"1.jpg",
+        corpus:"black",
+        remen:"black",
+        displaySize:"0.96\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 4,2",
+            os2: "iOS 8",
+        }
+    },
+    {
+        name:"22",
+        imgSrc:"2.jpg",
+        corpus:"pinc",
+        remen:"pinc",
+        displaySize:"0.6\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 5,2",
+            os2: "iOS 9",
+        }
+    },
+    {
+        name:"33",
+        imgSrc:"3.jpg",
+        corpus:"black",
+        remen:"black",
+        displaySize:"0.6\"",
+        displayType:"OLED",
+        bluetooth:true,
+        OS: {
+            os1: "Android 6,2",
+            os2: "iOS 10",
+        }
+    },
+];
